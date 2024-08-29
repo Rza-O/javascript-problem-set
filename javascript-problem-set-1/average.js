87093c190bd2de65fee07e1f83b2dbabf391b4b4
appoclayptor <shahreza1414@gmail.com>
2024-08-29 15:04:47 +0600

    testing

---
+++ javascript-problem-set-1/average.js
@@ -0,0 +1,11 @@
+var mathResult = 75.25;
+var biologyResult = 65;
+var chemistryResult = 80;
+var physicsResult = 35.45;
+var banglaResult = 99.50;
+
+var sum = mathResult + biologyResult + chemistryResult + physicsResult + banglaResult;
+var count = 5;
+var average = sum / count;
+
+console.log(sum);
