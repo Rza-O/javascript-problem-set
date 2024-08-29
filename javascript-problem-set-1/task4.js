558f3c425acb362d18cb27cc6dd3410e71968695
appoclayptor <shahreza1414@gmail.com>
2024-08-29 15:17:44 +0600

    Task4 explained

---
+++ javascript-problem-set-1/task4.js
@@ -0,0 +1,11 @@
+var a = isNaN('11');
+var b = isNaN(2-10)
+var c = isNaN('my name is shah reza')
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+// Output comes false
+// isNaN checks the given value to be a number or not;
+// isNaN function returns in boolean value 
