f3a33bb6df9d7e3441f588d6824711152df01a0c
appoclayptor <shahreza1414@gmail.com>
2024-08-29 14:12:04 +0600

    First problem sets added

---
+++ javascript-problem-set-1/supermarket.js
@@ -0,0 +1,8 @@
+var myMoney = 1200;
+var applePrice = 650;
+var orangePrice = 400;
+
+var sumPrice = applePrice + orangePrice;
+var returnMoney = myMoney - sumPrice;
+
+console.log(returnMoney+"tk")
